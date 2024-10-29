46bab94c4b468ef67fdda3202444c8a9c418a630
zoran.velichkovski <trendo@hotmail.com>
2024-10-29 16:06:19 +0100

    --- small changes

--- src/main/scala/BasixMixinExample.scala
+++ src/main/scala/BasixMixinExample.scala
@@ -1,10 +1,17 @@
 trait Greeting:
   def greet(name: String): String = s"Hello, $name!"
 
+trait Checking:
+  def check(name: String): String => Int = (s: String) => s.length
+
 class Person(val name: String)
 
-class FriendlyPerson(name: String) extends Person(name) with Greeting
+class FriendlyPerson(name: String) extends Person(name) with Greeting with Checking
 
 @main def runBasicMixinExample(): Unit =
   val person = new FriendlyPerson("Alice")
+  val lengthOfName = person.check(person.name)(person.name)
+
+  println("Length of name of Person is " + lengthOfName)
+
   println(person.greet(person.name))
